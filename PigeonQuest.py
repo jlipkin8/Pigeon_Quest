29695defd6f4ce620d5fb3765f6e152d30e06b8e
Josie Lipkin <jlipkin8@gmail.com>
2017-05-14 14:16:37 -0700

    added the skeletons for the functions: story_intro, pigeon_options, fly, rest, poop, eat

--- PigeonQuest.py
+++ PigeonQuest.py
@@ -6,3 +6,23 @@
 #              \\/     \_______\n\
 #              '        _======>\n\
 #              `'----\\`        "
+
+def story_intro(): 
+    # let the user know they are playing a pigeon 
+    # they are tasked with transporting a message from point A to point B 
+
+def pigeon_options(): 
+    # asks user for input 
+        # fly 
+        # rest 
+        # poop 
+        # eat 
+
+def fly(): 
+
+def rest(): 
+
+def poop(): 
+
+def eat(): 
+
