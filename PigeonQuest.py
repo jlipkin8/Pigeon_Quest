d563d8759bc4efb86efc0101ca3de4289ad2051c
Josie Lipkin <jlipkin8@gmail.com>
2017-05-14 14:28:38 -0700

    added the word pass to the function bodies so I wouldn't get any indent errors

--- PigeonQuest.py
+++ PigeonQuest.py
@@ -1,28 +1,37 @@
-# print            "\n _,-' ______\n \
-#                 .'  .-'  ____7\n\
-#                /   /   ___7\n\
-#              _|   /  ___7\n\
-#            >(')\ | ___7     \n\
-#              \\/     \_______\n\
-#              '        _======>\n\
-#              `'----\\`        "
 
 def story_intro(): 
     # let the user know they are playing a pigeon 
-    # they are tasked with transporting a message from point A to point B 
+    # they are tasked with transporting a message from point A to point B
+    pass
+
 
 def pigeon_options(): 
     # asks user for input 
-        # fly 
-        # rest 
-        # poop 
-        # eat 
+    # fly 
+    # rest 
+    # poop 
+    # eat 
+    # print "Dear Pigeon, \n\
+    # Would you like to: \n\
+    # 1. Fly \n\
+    # 2. Rest \n\
+    # 3. Poop \n\
+    # 4. Eat \n\
+    # 5. Quit this Mission \n\
+    # Enter 1, 2, 3, 4, or 5: "
+    pass
+
+# testing out the pigeon_options 
 
 def fly(): 
+    pass
 
 def rest(): 
+    pass
 
 def poop(): 
+    pass
 
-def eat(): 
+def eat():
+    pass
 
