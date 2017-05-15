fa8279a57cfb813bb9c09b48f2ad2fefc2faac59
Josie Lipkin <jlipkin8@gmail.com>
2017-05-14 17:32:15 -0700

    I have a minimum viable product. I have added vitals that need to be kept at certain levels

--- PigeonQuest.py
+++ PigeonQuest.py
@@ -1,37 +1,113 @@
 
+import time 
+
 def story_intro(): 
     # let the user know they are playing a pigeon 
     # they are tasked with transporting a message from point A to point B
-    pass
+    print "PIGEON QUEST"
+    time.sleep(.5)
+    print "Welcome to PIGEON QUEST"
+    print "YOU are a pigeon" 
+    time.sleep(.5)
+    print "You must transport a very important message from Point A to Point B."
+    time.sleep(.5)
+    print "Are you ready?!!!!"
 
+#testing out the story_intro 
+story_intro() 
 
-def pigeon_options(): 
+def display_main_menu(vitals): 
     # asks user for input 
     # fly 
     # rest 
     # poop 
     # eat 
-    # print "Dear Pigeon, \n\
-    # Would you like to: \n\
-    # 1. Fly \n\
-    # 2. Rest \n\
-    # 3. Poop \n\
-    # 4. Eat \n\
-    # 5. Quit this Mission \n\
-    # Enter 1, 2, 3, 4, or 5: "
-    pass
+    time.sleep(1)
+    pigeon_options = "\n\n\nDear Pigeon, \n\
+    Would you like to: \n\
+    1. Fly \n\
+    2. Rest \n\
+    3. Poop \n\
+    4. Eat \n\
+    5. Quit this Mission \n\
+    Enter 1, 2, 3, 4, or 5: "
 
-# testing out the pigeon_options 
+    while True: 
+        if vitals["distance"] == 5: 
+            print "Congratulations you've made it to Point B!"
+            break
+        elif vitals["poop_capacity"] > 4: 
+            print "I recommend you poop"
+        elif vitals["health"] < 3: 
+            print "I recommend you eat or rest"
 
-def fly(): 
-    pass
+        pigeon_input = raw_input(pigeon_options)
+        if (pigeon_input == "1") and vitals["health"] >= 3: 
+            print "fly"
+            fly(vitals)
+        elif pigeon_input == "2": 
+            print "rest"
+            rest(vitals)
+        elif pigeon_input == "3": 
+            print "Poop"
+            poop(vitals)
+        elif pigeon_input == "4": 
+            print "Eat"
+            eat(vitals)
+        elif pigeon_input == "5": 
+            break 
+        else: 
+            print "invalid option, please enter 1, 2, 3, 4, or 5"
 
-def rest(): 
-    pass
 
-def poop(): 
-    pass
+def fly(vitals): 
+    vitals["health"] = vitals["health"] - 1
+    vitals["distance"] = vitals["distance"] + 1 
 
-def eat():
-    pass
+
+def rest(vitals): 
+    vitals["health"] = vitals["health"] + 1 
+
+def poop(vitals): 
+    vitals["poop_capacity"] = vitals["poop_capacity"] - 1
+
+def eat(vitals):
+    vitals["health"] = vitals["health"] + 1 
+    vitals["poop_capacity"] = vitals["poop_capacity"] + 1
+
+
+vitals = {
+    "health": 5,
+    "distance": 0, 
+    "poop_capacity": 3
+}
+display_main_menu(vitals)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
